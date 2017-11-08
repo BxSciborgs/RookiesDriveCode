906b654cde370af6066d47becdce39dd3f9b7121
Shuyu Cao <caos1@bxscience.edu>
2017-11-08 16:13:20 -0500

    Fixed Talons, changed to CANTalons

--- src/org/usfirst/frc/team1155/robot/subsystems/TankDriveSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/TankDriveSubsystem.java
@@ -1,21 +1,24 @@
 package org.usfirst.frc.team1155.robot.subsystems;
 
+import org.usfirst.frc.team1155.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class TankDriveSubsystem extends Subsystem{
-	
-	Talon leftTalon = new Talon(1);
-	Talon rightTalon = new Talon(2);
+	CANTalon leftCantalon = new CANTalon(RobotMap.leftMotor);
+	CANTalon rightCantalon = new CANTalon(RobotMap.rightMotor);	
 	@Override
 	protected void initDefaultCommand() {
-		leftTalon.setSpeed(0);
-		rightTalon.setSpeed(0);
+		leftCantalon.set(0);
+		rightCantalon.set(0);
 	}
 	
 	public void setSpeed(double leftSpeed, double rightSpeed)
 	{
-		leftTalon.setSpeed(leftSpeed);
-		rightTalon.setSpeed(rightSpeed);
+		leftCantalon.set(leftSpeed);
+		rightCantalon.set(rightSpeed);
 	}
 }
